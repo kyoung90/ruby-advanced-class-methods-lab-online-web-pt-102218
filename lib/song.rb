98347517113e2054174017e3adebcbed9434509f
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-11-15 17:36:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,6 +42,7 @@ class Song
   def self.find_or_create_by_name(name)
     found = self.find_by_name(name)
     if found
+      binding.pry
       return found
     end 
     self.create_by_name(name)
