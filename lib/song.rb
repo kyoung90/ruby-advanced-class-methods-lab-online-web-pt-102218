35a5f8182465504e3ea214b9afb655d62f2ec4da
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-11-15 17:02:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,6 +30,10 @@ class Song
   end 
   
   def self.find_by_name(name)
-    @@all.select{|song| song.name == name}
+    @@all.each do |song|
+      if (song.name == name)
+        return song
+      end 
+    end 
   end 
 end
